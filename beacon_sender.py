50bd96c4a1eefdd6258ee13b5d93600dca1a1adb
Ido Shamay <ido.shamay@stratoscale.com>
2016-02-08 18:12:31 +0200

    Added beacon sender

---
+++ beacon_sender.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+import netifaces
+import time
+
+###########################################################################
+###########################################################################
+
+class BeaconSender (threading.Thread):
+    def __init__(self, port, key, interface):
+        threading.Thread.__init__(self)
+        self._port = port
+        self._key = key
+        self._interface = interface
+        self._sendEvent = threading.Event()
+        self.quit = False
+
+    ################################################
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        assert(self._interface in netifaces.interfaces())
+        ipv4_addr = netifaces.ifaddresses(self._interface)[netifaces.AF_INET]["addr"]
+        sock.bind(ipv4_addr)
+        bcast_addr = netifaces.ifaddresses(self._interface)[netifaces.AF_INET]["broadcast"]
+
+        while not self.quit:
+            try:
+                sock.sendto(self._key, (bcast_addr, self._port))
+                
+            except socket.error as exc:
+                print "Socket error ... continuing anyway"
+                continue
+            
+            time.sleep(1)
+        sock.close()
