00d80e4a2f786a3e141569b98201a6b79a2cab5a
Ido Shamay <ido.shamay@stratoscale.com>
2016-02-09 11:46:03 +0200

    Added pcap loop for beacon vlan receiver

--- vlan_beacon.py
+++ vlan_beacon.py
@@ -11,6 +11,7 @@ SNAPLEN = 1024
 TIMEOUT = 2000
 BEACON_UDP_PORT = 12000
 KEY = 'abc'
+MAX_PKTS = -1
 
 ###########################################################################
 ###########################################################################
@@ -26,27 +27,21 @@ class VlanBeacon(threading.Thread):
     ################################################
 
     def run(self):
+        # the method which will be called when a packet is captured
+        def _parse_packet(hdr, data):
+            print ('%s: captured %d bytes, truncated to %d bytes' % (datetime.datetime.now(), hdr.getlen(), hdr.getcaplen()))
+            eth = EthDecoder().decode(data)
+            assert(isinstance(eth.child(), ImpactPacket.IP))
+            ip = eth.child()
+            assert(isinstance(ip.child(), ImpactPacket.UDP))
+            udp = ip.child()
+            assert(udp.get_uh_dport(), self._port)
+            payload = udp.child()
+            if payload.get_buffer_as_string() == 'abc':
+                print 'received beacon from %s' %  ip.get_ip_src()
+                if eth.tag_cnt > 0:
+                    print "vlan %d" % eth.get_tag(0).get_vid()
+
         cap = pcapy.open_live(self._device, SNAPLEN, False, TIMEOUT)
         cap.setfilter('udp port %s' % self._port)
-
-        while not self.quit:
-            try:
-                (header, packet) = cap.next()
-            except pcapy.PcapError:
-                print ('%s: Timeout without a beacon ... continueing anyway' % datetime.datetime.now())
-                continue
-
-            print ('%s: captured %d bytes, truncated to %d bytes' % (datetime.datetime.now(), header.getlen(), header.getcaplen()))
-            self._parse_packet(packet)
-
-    def _parse_packet(self, packet):
-        eth = EthDecoder().decode(packet)
-        assert(isinstance(eth.child(), ImpactPacket.IP))
-        ip = eth.child()
-        assert(isinstance(ip.child(), ImpactPacket.UDP))
-        udp = ip.child()
-        assert(udp.get_uh_dport(), BEACON_UDP_PORT)
-        payload = udp.child()
-        print payload.get_buffer_as_string()
-        if eth.tag_cnt > 0:
-            print "vlan %d" % eth.get_tag(0).get_vid()
+        cap.loop(MAX_PKTS, _parse_packet)
